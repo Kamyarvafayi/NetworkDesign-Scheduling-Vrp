1342145fdfee12b8e68b602d39e8349ab4b9f890
Kamy Vafyai <108336935+Kamyarvafayi@users.noreply.github.com>
2022-08-11 21:47:23 +0430

    Update Capacitated_Vrp.py

--- Capacitated_Vrp.py
+++ Capacitated_Vrp.py
@@ -15,7 +15,8 @@ class Classic_VRP_Capacitated:
         return Vehicle1*Random_Coeff + Vehicle2*(1-Random_Coeff), Vehicle2*Random_Coeff + Vehicle1*(1-Random_Coeff)
     def Vehicle_Mutation(self, Vehicle):
         Mutation_Size = np.random.randint(3,self.Input.shape[0]+1)
-        Mutation_Size = int(max(np.ceil(self.Input.shape[0]/6), 3))
+        Mutation_Size = int(max(np.ceil(self.Input.shape[0]/20), 3))
+        Mutation_Size = self.Mutation_Size
         Mutation_Indices = random.sample(range(self.Input.shape[0]), Mutation_Size)
         
         New_Vehicle = Vehicle.copy()
@@ -35,7 +36,8 @@ class Classic_VRP_Capacitated:
         return City_Order1_Copy , City_Order2_Copy
     def City_Order_Mutation(self, City_Order):
         Mutation_Speed = 0.2
-        Mutation_Size = int(max(np.ceil(self.Input.shape[0]/6), 3))
+        Mutation_Size = int(max(np.ceil(self.Input.shape[0]/20), 3))
+        Mutation_Size = self.Mutation_Size
         Mutation_Indices = random.sample(range(self.Input.shape[0]), Mutation_Size)
         New_Order = City_Order.copy()
         for City_Index in Mutation_Indices:
@@ -76,7 +78,6 @@ class Classic_VRP_Capacitated:
             Capacity_Cost += np.abs(min(self.Vehicle_Capacity[Vehicle-1]-np.sum(self.City_Demand[Vehicle_Order == Vehicle]), 0))
             #print(np.abs(self.Vehicle_Capacity[Vehicle-1]-np.sum(self.City_Demand[Vehicle_Order == Vehicle])))
         return Cost_Of_Vehicle + Transportation_Cost + self.Capacity_Violation_Cost*Capacity_Cost
-
     def Fit_Classic_Vrp(self, Input, Vehicle_Cost, City_Demand, Vehicle_Capacity, Capacity_Violation_Cost = 1000):
         self.Input = Input
         self.Vehicle_Cost = Vehicle_Cost
@@ -93,6 +94,7 @@ class Classic_VRP_Capacitated:
         self.Population_Vehicle = self.Vehicle_Initial_Solution[np.argsort(self.Objectives)]
         self.Population_City = self.City_Orders_Initial_Solution[np.argsort(self.Objectives)]
         self.Objectives_Sorted = np.sort(self.Objectives)
+        Mutation_rep = 1
         for i in range(self.Iter_Number):
             Vehicle_Cross_Over_solutions = self.Population_Vehicle.copy()
             Cities_Cross_Over_solutions = self.Population_City.copy()
@@ -100,7 +102,7 @@ class Classic_VRP_Capacitated:
             Cities_Mutation_solutions = self.Population_City.copy()
 
          #Crossover
-            Cross_Over_Indices = random.sample(range(0,self.Population_Size),30)
+            Cross_Over_Indices = random.sample(range(0,self.Population_Size),20)
             Cross_Over_Indices = np.append(Cross_Over_Indices,np.arange(10))
             Cross_Over_Indices2 = Cross_Over_Indices.copy()
             for j in Cross_Over_Indices:
@@ -116,8 +118,18 @@ class Classic_VRP_Capacitated:
                     self.Population_Vehicle =  np.append(self.Population_Vehicle, np.array([Vehicle_Cross_Over_solutions[k]]), axis=0)
             
         # Mutation
+            self.Mutation_Size = 5
             Mutation_Indices = random.sample(range(0,self.Population_Size),int(self.Population_Size*0.8))
-            for k in range(1):
+            for k in range(Mutation_rep):
+                if i>100:
+                    self.Mutation_Size = 3
+                    Mutation_rep = 3
+                    if i>200:
+                        self.Mutation_Size = 2
+                        Mutation_rep = 4
+                        if i >350:
+                            self.Mutation_Size = 1
+                            Mutation_rep = 15
                 for j in Mutation_Indices:
                     self.Population_Vehicle =  np.append(self.Population_Vehicle, np.array([self.Vehicle_Mutation(Vehicle_Mutation_solutions[j])]), axis=0)
                     self.Population_City =  np.append(self.Population_City, np.array([Cities_Cross_Over_solutions[j]]), axis=0)
@@ -140,23 +152,23 @@ class Classic_VRP_Capacitated:
             print("The optimal cost in Iteration {} is: ".format(i+1),self.Objectives_Sorted[0])
 # In[]:
 np.random.seed(seed = 1)
-Sample_Number = 60
+Sample_Number = 30
 Locations = np.random.uniform(0,100,size = [Sample_Number, 2])
 Input3 = np.array([[np.linalg.norm(Locations[i]-Locations[j]) for i in range(Sample_Number)] for j in range(Sample_Number)])
 Depot_Position = np.average(Locations, axis = 0)
-Vehicle_Number2 = 20
-Vehicle_Capacity = np.array([11195 for Vehicle in range(Vehicle_Number2)])
+#Depot_Position = np.array([0,0])
+Vehicle_Number2 = 5
+Vehicle_Capacity = np.array([50 for Vehicle in range(Vehicle_Number2)])
 np.random.seed(seed = 1)
-City_Demand = np.random.randint(6,7,size = Sample_Number, dtype = int)
+City_Demand = np.random.randint(50,51,size = Sample_Number, dtype = int)
 City_Demand[0] = 50
-City_Demand[1] = 32
-City_Demand[2] = 32
+City_Demand[1] = 50
+City_Demand[2] = 50
 np.random.seed(seed = 1)
-Vehicle_Cost3 = np.random.randint(0,1,size = [Vehicle_Number2,Sample_Number])
+Vehicle_Cost3 = np.random.randint(1,100,size = [Vehicle_Number2,Sample_Number])
 for i in range(Sample_Number):
     Input3[i,i] = np.linalg.norm(Depot_Position-Locations[i]) 
-
-VRP_Object3 = Classic_VRP_Capacitated(Iteration = 500, Population_Size = 500, Vehicle_Num=Vehicle_Number2)
+VRP_Object3 = Classic_VRP_Capacitated(Iteration = 800, Population_Size = 50, Vehicle_Num=Vehicle_Number2)
 Vrp3 = VRP_Object3.Fit_Classic_Vrp(Input3, Vehicle_Cost3, Vehicle_Capacity=Vehicle_Capacity, City_Demand=City_Demand)
 
 Vrp_City3 = VRP_Object3.Population_City
@@ -167,6 +179,7 @@ City_Sorted3 = City_Sorted3[np.argsort(Vrp_City3[0])]
 print(Vehicle_Sorted3)
 print(City_Sorted3)
 Vehicles = Vehicle_Sorted3[np.argsort(City_Sorted3)]
+
 # In[]:
 import matplotlib.pyplot as plt
 plt.figure()
@@ -181,7 +194,7 @@ for i in range(1,Vehicle_Number2+1):
     plt.scatter(Locations[Vehicles==i,0],Locations[Vehicles==i,1], marker = '>',c = color[i-1])
 for Points in range(Sample_Number):
     plt.text(Locations[Points,0]+0.02, Locations[Points,1]+0.02, '{}'.format(Points+1))
-plt.scatter(Depot_Position[0],Depot_Position[1], marker = 's', linewidths = 7, c = [1,0.5,0.3])
+plt.scatter(Depot_Position[0],Depot_Position[1], marker = 's', linewidths = 10, c = [1,0.5,0.3])
 plt.text(Depot_Position[0]+0.02,Depot_Position[1]+0.02, "Depot")
 for i in range(1,Vehicle_Number2+1):
     Locations_Copy = Locations.copy()
@@ -193,8 +206,6 @@ for i in range(1,Vehicle_Number2+1):
     Vehicle_Sorted3_Copy = Vehicle_Sorted3.copy()
     Vehicle_Sorted3_Copy = np.insert(Vehicle_Sorted3_Copy,0,np.array([i]),axis = 0)
     Vehicle_Sorted3_Copy = np.append(Vehicle_Sorted3_Copy,np.array([i]),axis = 0)
-    #plt.plot(Locations[origin,:],Locations[City_Sorted3[j]-1,:], color = color[i])
-    #plt.plot(Locations[City_Sorted3[Vehicle_Sorted3==i]-1,0],Locations[City_Sorted3[Vehicle_Sorted3==i]-1,1], color = color[i])
     plt.plot(Locations_Copy[City_Sorted3_Copy[Vehicle_Sorted3_Copy==i],0],Locations_Copy[City_Sorted3_Copy[Vehicle_Sorted3_Copy==i],1],'-', color = color[i-1])
     plt.plot(Locations_Copy[0,0],Locations_Copy[0,1], color = color[i-1])
     plt.show()
